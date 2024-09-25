90785e3bd08c33ba2e2242388d4100613382834a
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-09-24 20:32:48 -0500

    Update model-instructors.php

--- model-instructors.php
+++ model-instructors.php
@@ -2,7 +2,8 @@
 function selectInstructors() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("");
+        $stmt = $conn->prepare("select instructor_id, instructor_name, office_number
+from `mis4013-hw3`.instructor");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
