eacc1185a7de91074938097ef46ddaa45b5de73b
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-09-24 20:42:07 -0500

    Create view-instructors.php

---
+++ view-instructors.php
@@ -0,0 +1,27 @@
+<h1>
+  Instructors
+</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Office</th>
+      </tr>
+    </thead>
+    <tbody>
+  <?php 
+    while ($instrcutor=$instructors->fetch_assoc()) {
+      ?>
+      <tr>
+        <td><?php echo $instrcutor['instructor_id'];?></td>
+        <td>?php echo $instrcutor['instructor_name'];?></td>
+        <td>?php echo $instrcutor['office_number'];?></td>
+      </tr>
+      <?php
+    }
+  ?>
+    </tbody>
+  </table>
+</div>
