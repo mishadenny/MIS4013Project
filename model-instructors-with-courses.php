f8c6fc0a613e9210d58885607846e764a915a893
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-10-01 22:20:43 -0500

    Update model-instructors-with-courses.php

--- model-instructors-with-courses.php
+++ model-instructors-with-courses.php
@@ -13,4 +13,21 @@ from `mis4013-hw3`.instructor");
         throw $e;
     }
 }
+
+function selectCoursesByInstructor($iid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("select c.course_id, course_number, course_description, semester, room, day_time
+from `mis4013-hw3`.course c 
+join `mis4013-hw3`.section s on s.course_id=c.course_id where s.instructor_id=?");
+        $stmt->bind_param("i", $iid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
