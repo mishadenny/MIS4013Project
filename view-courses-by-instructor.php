fc72906db260ed9321521a676c361b0b505bb8ae
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-10-01 21:41:44 -0500

    Create view-courses-by-instructor.php

---
+++ view-courses-by-instructor.php
@@ -0,0 +1,33 @@
+<h1>
+  Courses By Instructor
+</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Number</th>
+      <th>Description</th>
+      <th>Semester</th>
+      <th>Room</th>
+      <th>Day/Time</th>
+      </tr>
+    </thead>
+    <tbody>
+  <?php 
+    while ($course=$courses->fetch_assoc()) {
+      ?>
+      <tr>
+        <td><?php echo $course['course_id']; ?></td>
+        <td><?php echo $course['course_number'];?></td>
+        <td><?php echo $course['course_description'];?></td>
+        <td><?php echo $course['semester'];?></td>
+        <td><?php echo $course['room'];?></td>
+        <td><?php echo $course['day_time'];?></td>
+      </tr>
+      <?php
+    }
+  ?>
+    </tbody>
+  </table>
+</div>
