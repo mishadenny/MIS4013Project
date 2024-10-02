394387259d6a23740147920eb490f99cadfabeb9
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-10-01 21:54:45 -0500

    Update view-instructors.php

--- view-instructors.php
+++ view-instructors.php
@@ -19,7 +19,7 @@
         <td><?php echo $instructor['instructor_id']; ?></td>
         <td><?php echo $instructor['instructor_name'];?></td>
         <td><?php echo $instructor['office_number'];?></td>
-        <td><a href="courses-by-instructors.php?id=<?php echo $instructor['instructor_id']; ?>">Courses</a></td>
+        <td><a href="courses-by-instructor.php?id=<?php echo $instructor['instructor_id']; ?>">Courses</a></td>
       </tr>
       <?php
     }
