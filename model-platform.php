529ec2ad99fa71423de235f16b5f361041e16cb5
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-10-05 11:39:51 -0500

    Create model-platform.php

---
+++ model-platform.php
@@ -0,0 +1,16 @@
+<?php
+function selectPlatform() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT platform_id, platform_name, headquarters
+FROM `mis4013-hw3`.platform;w");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
