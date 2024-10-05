930cafa55f356b297dd166e135782a1b8784f303
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-10-05 14:02:18 -0500

    Create model-shows-by-platform.php

---
+++ model-shows-by-platform.php
@@ -0,0 +1,19 @@
+<?php
+function selectShowsByPlatform($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("select p.platform_id, platform_name, show_title, genre
+from `mis4013-hw3`.platform p
+join `mis4013-hw3`.show s on s.platform_id=p.platform_id
+where s.show_id=?");
+        $stmt->bind_param("i", $cid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
