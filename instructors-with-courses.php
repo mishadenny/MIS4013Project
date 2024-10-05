91c35eb6880be73f95e8f77f8c9d63c164094574
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-10-05 13:32:58 -0500

    Update instructors-with-courses.php

--- instructors-with-courses.php
+++ instructors-with-courses.php
@@ -2,7 +2,7 @@
 require_once("util-db.php");
 require_once("model-instructors-with-courses.php");
 
-$pageTitle ="Instructors With Courses";
+$pageTitle ="Actors with Shows";
 include "view-header.php"; 
 $instructors = selectInstructors();
 include "view-instructors-with-courses.php";
