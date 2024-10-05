b9c205b2fa78761148060584f63eff0288e2f466
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-10-05 10:46:15 -0500

    Update view-courses.php

--- view-courses.php
+++ view-courses.php
@@ -1,13 +1,13 @@
 <h1>
-  Courses
+  Shows
 </h1>
 <div class="table-responsive">
   <table class="table">
     <thead>
       <tr>
       <th>ID</th>
-      <th>Number</th>
-      <th>Description</th>
+      <th>Title</th>
+      <th>Genre</th>
       <th></th>
       </tr>
     </thead>
@@ -16,13 +16,13 @@
     while ($course=$courses->fetch_assoc()) {
       ?>
       <tr>
-        <td><?php echo $course['course_id']; ?></td>
-        <td><?php echo $course['course_number'];?></td>
-        <td><?php echo $course['course_description'];?></td>
+        <td><?php echo $course['show_id']; ?></td>
+        <td><?php echo $course['show_title'];?></td>
+        <td><?php echo $course['show_genre'];?></td>
         <td>
           <form method="post" action="sections-by-course.php">
-            <input type="hidden" name="cid" value="<?php echo $course['course_id']; ?>">
-            <button type="submit" class="btn btn-primary">Sections</button>
+            <input type="hidden" name="cid" value="<?php echo $course['show_id']; ?>">
+            <button type="submit" class="btn btn-primary">Episodes</button>
           </form>
         </td>
       </tr>
