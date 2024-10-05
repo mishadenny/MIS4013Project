e6e08c91d9568b60057ba2ef99ff88bebe4af03d
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-10-05 11:42:27 -0500

    Create view-platform.php

---
+++ view-platform.php
@@ -0,0 +1,34 @@
+<h1>
+  Platforms
+</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Headquarters</th>
+      <th></th>
+      </tr>
+    </thead>
+    <tbody>
+  <?php 
+    while ($platform=$platforms->fetch_assoc()) {
+      ?>
+      <tr>
+        <td><?php echo $platform['platform_id']; ?></td>
+        <td><?php echo $platform['platform_name'];?></td>
+        <td><?php echo $platform['headquarters'];?></td>
+        <td>
+          <form method="post" action="shows-by-platform">
+            <input type="hidden" name="cid" value="<?php echo $platform['platform_id']; ?>">
+            <button type="submit" class="btn btn-primary">shows</button>
+          </form>
+        </td>
+      </tr>
+      <?php
+    }
+  ?>
+    </tbody>
+  </table>
+</div>
