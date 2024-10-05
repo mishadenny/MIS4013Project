1a210f0445d62015df25d2ee5833f7437526a0e7
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-10-05 12:54:05 -0500

    Update courses-by-instructor.php

--- courses-by-instructor.php
+++ courses-by-instructor.php
@@ -2,7 +2,7 @@
 require_once("util-db.php");
 require_once("model-courses-by-instructor.php");
 
-$pageTitle ="Courses by Instructor";
+$pageTitle ="Shows by Actors";
 include "view-header.php"; 
 $courses = selectCoursesByInstructor($_GET['id']);
 include "view-courses-by-instructor.php";
