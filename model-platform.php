8697f3ea63590f2815fccd57d47aa47a9d7955ff
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-10-05 11:40:09 -0500

    Update model-platform.php

--- model-platform.php
+++ model-platform.php
@@ -3,7 +3,7 @@ function selectPlatform() {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT platform_id, platform_name, headquarters
-FROM `mis4013-hw3`.platform;w");
+FROM `mis4013-hw3`.platform;");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
