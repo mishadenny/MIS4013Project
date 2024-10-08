405ba2db91aa536d06b1d2df871c500b5166bf2c
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-10-08 18:17:01 -0500

    Update courses.php

--- courses.php
+++ courses.php
@@ -8,7 +8,10 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      InsertShow($_POST['sTitle'], $_POST['sGenre']);
+      If (InsertShow($_POST['sTitle'], $_POST['sGenre'])) (
+        echo '<div class="alert alert-success" role="alert">Course Added </div>';
+  } else {
+    echo '<div class="alert alert-danger" role="alert">Error </div>';
       break;
   }
 }
