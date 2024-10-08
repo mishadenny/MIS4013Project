e65aef91ccf147b6bd20be1fc86ccdc2e87f711c
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-10-08 18:11:40 -0500

    Update courses.php

--- courses.php
+++ courses.php
@@ -6,11 +6,11 @@ $pageTitle ="Shows";
 include "view-header.php"; 
 
 if (isset($_POST['actionType'])) {
-  switch($_POST['actionType']) {
+  switch ($_POST['actionType']) {
     case "Add":
-      InsertShow($_POST['sTitle'], $_POST['cGenre']);
+      InsertShow($_POST['sTitle'], $_POST['sGenre']);
       break;
-  {
+  }
 }
 $courses = selectCourses();
 include "view-courses.php";
