fa3894c169c9dab5109fad9356d71575c0f90300
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-10-08 18:00:59 -0500

    Update view-shows-newform.php

--- view-shows-newform.php
+++ view-shows-newform.php
@@ -24,6 +24,7 @@
             <label for="sGenre" class="form-label">Show Genre</label>
             <input type="text" class="form-control" id="sGenre" name="sGenre">
           </div>
+              <input type="hidden" name="actionType" value="Add">
           <button type="submit" class="btn btn-primary">Save</button>
         </form>
       </div>
