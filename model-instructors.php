920af8d9544439bb1a88919fa11fa4123b1a1c75
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-10-14 13:36:33 -0500

    Update model-instructors.php

--- model-instructors.php
+++ model-instructors.php
@@ -13,4 +13,49 @@ from `mis4013-hw3`.actor");
         throw $e;
     }
 }
+
+function insertActor($aName, $aAge) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `mis4013-hw3`.`actor` (`actor_name`, `age`) VALUES (?, ?)");
+        $stmt->bind_param("si", $aName, $aAge);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateActor($aName, $aAge, $iid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `mis4013-hw3`.`actor` set `actor_name`=?, `age`=? where actor_id=?");
+        $stmt->bind_param("sii", $aName, $aAge, $iid);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteActor($iid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from `mis4013-hw3`.`actor` where actor_id=?");
+        $stmt->bind_param("i", $iid);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
