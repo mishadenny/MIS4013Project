631f66ac7eca79eb0c4b94b0d7e7229b02c98891
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-10-14 21:24:24 -0500

    Update instructors.php

--- instructors.php
+++ instructors.php
@@ -4,6 +4,33 @@ require_once("model-instructors.php");
 
 $pageTitle ="Actors";
 include "view-header.php"; 
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertActor($_POST['aName'], $_POST['aAge'])) {
+        echo '<div class="alert alert-success" role="alert">Actor Added </div>';
+  } else {
+    echo '<div class="alert alert-danger" role="alert">Error </div>';
+      }
+      break;
+    case "Edit":
+      if (updateActor($_POST['aName'], $_POST['aAge'], $_POST['iid'])) {
+        echo '<div class="alert alert-success" role="alert">Actor updated </div>';
+  } else {
+    echo '<div class="alert alert-danger" role="alert">Error </div>';
+      }
+      break;
+    case "Delete":
+      if (deleteActor($_POST['iid'])) {
+        echo '<div class="alert alert-success" role="alert">Actor Deleted </div>';
+  } else {
+    echo '<div class="alert alert-danger" role="alert">Error </div>';
+      }
+      break;
+  }
+}
+
 $instructors = selectInstructors();
 include "view-instructors.php";
 include "view-footer.php";
