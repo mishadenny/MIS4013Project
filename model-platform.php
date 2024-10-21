1e197f56645adf93552db8cc10a9947d15183b80
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-10-21 00:43:30 -0500

    Update model-platform.php

--- model-platform.php
+++ model-platform.php
@@ -13,4 +13,49 @@ FROM `mis4013-hw3`.platform;");
         throw $e;
     }
 }
+
+function insertPlatform($pName, $pHeadquarters) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `mis4013-hw3`.`platform` (`platform_name`, `headquarters`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $pName, $pHeadquarters);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updatePlatform($pName, $pHeadquarters, $pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `mis4013-hw3`.`platform` set `platform_name`=?, `headquarters`=? where platform_id=?");
+        $stmt->bind_param("ssi", $pName, $pHeadquarters, $pid);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deletePlatform($pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from `mis4013-hw3`.`platform` where platform_id=?");
+        $stmt->bind_param("i", $pid);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
