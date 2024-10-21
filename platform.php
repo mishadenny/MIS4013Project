5a54fd7d763643dbfc07875cf7b46b43c6660fd5
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-10-21 00:53:56 -0500

    Update platform.php

--- platform.php
+++ platform.php
@@ -4,6 +4,32 @@ require_once("model-platform.php");
 
 $pageTitle ="Platforms";
 include "view-header.php"; 
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertPlatform($_POST['pName'], $_POST['pHeadquarters'])) {
+        echo '<div class="alert alert-success" role="alert">Platform Added </div>';
+  } else {
+    echo '<div class="alert alert-danger" role="alert">Error </div>';
+      }
+      break;
+    case "Edit":
+      if (updatePlatform($_POST['pName'], $_POST['pHeadquarters'], $_POST['cid'])) {
+        echo '<div class="alert alert-success" role="alert">Platform updated </div>';
+  } else {
+    echo '<div class="alert alert-danger" role="alert">Error </div>';
+      }
+      break;
+    case "Delete":
+      if (deletePlatform($_POST['cid'])) {
+        echo '<div class="alert alert-success" role="alert">Platform Deleted </div>';
+  } else {
+    echo '<div class="alert alert-danger" role="alert">Error </div>';
+      }
+      break;
+  }
+}
 $platforms = selectPlatform();
 include "view-platform.php";
 include "view-footer.php";
