2523a4306a9354b568675ef77098ad36d10e0497
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-10-21 20:17:30 -0500

    Create view-actor-input-list.php

---
+++ view-actor-input-list.php
@@ -0,0 +1,9 @@
+<select class="form-select" id="iid" name="iid">
+<?php
+while ($actorItem= $actorList->fetch_assoc()){
+?>
+  <option value="<?php echo $actorItem['actor_id']; ?>">"<?php echo $actorItem['actor_name']; ?></option>
+<?php
+}
+?>
+</select>
