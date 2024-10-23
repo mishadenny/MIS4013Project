4028aaa7672bf7ea926d89e8a54798f31cf9d1ba
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-10-23 13:49:20 -0500

    Update instructors-with-courses.php

--- instructors-with-courses.php
+++ instructors-with-courses.php
@@ -1,35 +1,37 @@
+<!-- PHP for handling form actions -->
 <?php
 require_once("util-db.php");
 require_once("model-instructors-with-courses.php");
 
-$pageTitle ="Actors with Shows";
-include "view-header.php"; 
+$pageTitle = "Actors with Shows";
+include "view-header.php";
 
 if (isset($_POST['actionType'])) {
-  switch ($_POST['actionType']) {
-    case "Add":
-      if (insertEpisode($_POST['iid'], $_POST['cid'], $_POST['titleepisode'], $_POST['seasonnumber'], $_POST['episodenumber'])) {
-        echo '<div class="alert alert-success" role="alert">Episode Added </div>';
-  } else {
-    echo '<div class="alert alert-danger" role="alert">Error </div>';
-      }
-      break;
-    case "Edit":
-      if (updateEpisode($_POST['iid'], $_POST['cid'], $_POST['titleepisode'], $_POST['seasonnumber'], $_POST['episodenumber'],$_POST['sid'])) {
-        echo '<div class="alert alert-success" role="alert">Epside updated </div>';
-  } else {
-    echo '<div class="alert alert-danger" role="alert">Error </div>';
-      }
-      break;
-    case "Delete":
-      if (deleteEpisode($_POST['sid'])) {
-        echo '<div class="alert alert-success" role="alert">Episoded Deleted </div>';
-  } else {
-    echo '<div class="alert alert-danger" role="alert">Error </div>';
-      }
-      break;
-  }
+    switch ($_POST['actionType']) {
+        case "Add":
+            if (insertEpisode($_POST['iid'], $_POST['cid'], $_POST['titleepisode'], $_POST['seasonnumber'], $_POST['episodenumber'])) {
+                echo '<div class="alert alert-success" role="alert">Episode Added Successfully</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error Adding Episode</div>';
+            }
+            break;
+        case "Edit":
+            if (updateEpisode($_POST['iid'], $_POST['cid'], $_POST['titleepisode'], $_POST['seasonnumber'], $_POST['episodenumber'], $_POST['sid'])) {
+                echo '<div class="alert alert-success" role="alert">Episode Updated Successfully</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error Updating Episode</div>';
+            }
+            break;
+        case "Delete":
+            if (deleteEpisode($_POST['sid'])) {
+                echo '<div class="alert alert-success" role="alert">Episode Deleted Successfully</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error Deleting Episode</div>';
+            }
+            break;
+    }
 }
+
 $instructors = selectInstructors();
 include "view-instructors-with-courses.php";
 include "view-footer.php";
